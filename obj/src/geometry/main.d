c170eb6bb13106a7e4cde4dc814b2aeb92f1a848
Kirill Ogarkov <Klipo07@example.com>
2023-04-26 01:24:35 +0700

    Update obj and bin files

--- obj/src/geometry/main.d
+++
@@ -1,2 +0,0 @@
-obj/src/geometry/main.o: src/geometry/main.c src/libgeometry/lab1.h
-src/libgeometry/lab1.h:
--- obj/src/libgeometry/lab1.d
+++
@@ -1,2 +0,0 @@
-obj/src/libgeometry/lab1.o: src/libgeometry/lab1.c src/libgeometry/lab1.h
-src/libgeometry/lab1.h:
