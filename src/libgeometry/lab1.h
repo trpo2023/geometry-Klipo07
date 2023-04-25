233b2fe8f3082dc71e2d62491ab660f14892778b
Kirill Ogarkov <Klipo07@example.com>
2023-04-25 23:58:24 +0700

    Update files

--- src/libgeometry/lab1.h
+++ src/libgeometry/lab1.h
@@ -1,38 +1,40 @@
 #pragma once
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
+#define MAX_INPUT_LENGTH 100
+#define MAX_OUTPUT_LENGTH 400
+#define MAX_CIRCLES 10
 
-#define RED_FLASH "\e[5;31m"
-#define END "\e[0m"
-#define MAX 100
+typedef struct Circle {
+    double x, y, r, perimetr, area;
+    int* intersects;
+} Circle;
 
-struct Point {
-    double x;
-    double y;
+enum {
+    ERROR_FILE_OUTPUT = 1,
+    ERROR_FILE_INPUT,
+    ERROR_PARSER_NAME,
+    ERROR_PARSER_LEFT_PARENTHESIS,
+    ERROR_PARSER_RIGHT_PARENTHESIS,
+    ERROR_PARSER_COMMA,
+    ERROR_PARSER_DOUBLE,
+    ERROR_PARSER_UNEXPECTED_TOKEN,
+    ERROR_REALLOC
 };
-
-struct Circle {
-    struct Point center;
-    double radius;
-};
-
-enum Bugs {
-    BUG_NAME,
-    BUG_STAPLES,
-    BUG_NUMBER,
-    BUG_UNIDENTIFIED_VARIABLES,
-    BUG_EXPECT_COMMA,
-    BUG_STAPLES_2,
-    BUG_EXTRA_POINT,
-};
-
-void output_bugs(int errors, int num, char* ch);
-void to_lower(char* str, int ch);
-double x_data(char* arr, int* num);
-double y_data(char* arr, int* num);
-double radius_data(char* arr, int* num);
-void empty(char* arr, int* num);
-struct Point find_center(char* arr, int* num);
-struct Circle find_out_circle(struct Point* Center, char* arr, int* num);
-void output_circle_message(struct Circle* circle);
+// Функция для проверки входных и выходных файлов
+int is_input_files_exist(const char* input_path, const char* output_path);
+// Функция для обработки ошибок и вывода сообщения
+void handle_error(int error_id, char* str);
+// Функция для перевода строки в нижний регистр
+char* lower_all(char* str);
+// Функции для парсинга круга
+int is_circle(char* str);
+int is_left_parenthesis(char* str);
+int is_double(char* str_start, char** str_end, double* num);
+int is_prefix(char* str_start, char* prefix);
+int is_num_circle(char* str_start, char** str_end, char* ending, double* x);
+// Функция для обработки входной строки
+void calculate_circle(Circle* circle);
+int parse_circle(char* str, Circle* out_values);
+// Функция для вывода круга
+void print_circles(const char* output_path, Circle* circles, int count);
+void get_intersects_circles(Circle* circles, int count);
+void free_circles(Circle* circles, int count);
--- test/parser_test.c
+++ test/parser_test.c
@@ -8,80 +8,130 @@
 
 #include <ctest.h>
 
-CTEST(TEST_output_bugs, outputs_correct_message) {
-    char message[] = "This is a test message";
-    output_bugs(1, 5, message);
-    ASSERT_STR("Error 1 occurred at location 5: This is a test message", message);
-}
-
-CTEST(TEST_to_lower, converts_string_to_lowercase) {
-    char str[] = "Hello, World!";
-    to_lower(str, 13);
-    ASSERT_STR("hello, world!", str);
-}
-
-CTEST(TEST_x_data, returns_correct_x_coordinate) {
-    int num = 0;
-    double result = x_data("(1.23 4.56, 3)", &num);
-    ASSERT_EQUAL(1, result);
-    ASSERT_EQUAL(5, num);
-    ASSERT_DBL_NEAR_TOL(1.23, result, 1e-6);
-}
-
-CTEST(TEST_y_data, returns_correct_y_coordinate) {
-    int num;
-    int result = y_data("1.23, 4.56, 3", &num);
-    ASSERT_EQUAL(1, result);
-    ASSERT_EQUAL(4, num);
-    ASSERT_DBL_NEAR_TOL(1, result, 1e-6);
-}
-
-CTEST(radius_data, valid_string)
+CTEST(TEST_is_input_files_exist, simple_file)
 {
-    char arr[] = "(1.23 4.56, 7.86)";
-    int num = 0;
-    double result = radius_data(arr, &num);
-    ASSERT_DBL_NEAR_TOL(7.86, result, 1e-6);
-    ASSERT_EQUAL(strlen(arr)-1, num);
+    char* wrong_file = "wrong.tmp";
+    char* right_file = "right.tmp";
+    char* output_file = "out.tmp";
+
+    FILE* create_file = fopen(right_file, "w");
+    ASSERT_EQUAL(0, is_input_files_exist(right_file, output_file));
+    fclose(create_file);
+    remove(right_file);
+
+    remove(wrong_file);
+    ASSERT_EQUAL(
+            ERROR_FILE_INPUT, is_input_files_exist(wrong_file, output_file));
+    remove(output_file);
 }
 
-/*
-CTEST(TEST_radius_data, returns_correct_radius) {
-    int num = 0;
-    double result = radius_data("(1.23 4.56, 7.86)", &num);
-    ASSERT_EQUAL(2.0, result);
-    ASSERT_EQUAL(3, num);
-}
-*/
-CTEST(TEST_empty, empties_string_and_resets_num) {
-    char str[] = "This is a test string";
-    int num = 5;
-    empty(str, &num);
-    ASSERT_STR("", str);
-    ASSERT_EQUAL(0, num);
+CTEST(TEST_lower_all, simple_lower)
+{
+    char bad[] = "1!2FsfT3rFl$km.Zzx";
+    char good[] = "1!2fsft3rfl$km.zzx";
+    char* result = lower_all(bad);
+
+    ASSERT_EQUAL(0, strcmp(good, result));
 }
 
-CTEST(find_center, returns_correct_center_point) {
-    int num;
-    struct Point result = find_center("1.23, 4.56; 7.89, 0.12; 3.45, 6.78", &num);
-    ASSERT_EQUAL(6, num);
-    ASSERT_DBL_NEAR_TOL(4.19, result.x, 1e-6);
-    ASSERT_DBL_NEAR_TOL(3.82, result.y, 1e-6);
+CTEST(TEST_is_circle, simple_circle)
+{
+    ASSERT_EQUAL(0, is_circle("circle"));
+    ASSERT_EQUAL(ERROR_PARSER_NAME, is_circle("not circle"));
 }
 
-CTEST(find_out_circle, returns_correct_circle) {
-    int num;
-    struct Point center = {1.23, 4.56};
-    struct Circle result = find_out_circle(&center, "2.34, 5.67; 3.45, 6.78; 4.56, 7.89", &num);
-    ASSERT_EQUAL(6, num);
-    ASSERT_DBL_NEAR_TOL(3.184474, result.radius, 1e-6);
-    ASSERT_DBL_NEAR_TOL(1.971825, result.center.x, 1e-6);
-    ASSERT_DBL_NEAR_TOL(5.392342, result.center.y, 1e-6);
+CTEST(TEST_is_double, simple_double)
+{
+    char *start = "25", *end;
+    double num = 0;
+    ASSERT_EQUAL(0, is_double(start, &end, &num));
+    ASSERT_EQUAL(25, num);
 }
 
-CTEST(output_circle_message, outputs_correct_message) {
-    struct Circle circle = {{1.23, 4.56}, 7.89};
-    char message[100];
-    output_circle_message(&circle);
-    ASSERT_STR("Circle with center (1.23, 4.56) and radius 7.89", message);
+CTEST(TEST_is_prefix, simple_prefix)
+{
+    char str[] = "Super text";
+    ASSERT_EQUAL(0, is_prefix(str, "Super"));
+    ASSERT_EQUAL(0, is_prefix(str + 6, "text"));
+    ASSERT_EQUAL(
+            ERROR_PARSER_UNEXPECTED_TOKEN, is_prefix(str, "not Super text"));
+}
+
+CTEST(TEST_is_num_circle, simple_num_circle)
+{
+    char str[] = "circle(1 5, 25)";
+    char* end;
+    double x = 0, y = 0, r = 0;
+
+    ASSERT_EQUAL(0, is_num_circle(str + 7, &end, " ", &x));
+    ASSERT_EQUAL(0, strcmp("5, 25)", end));
+
+    ASSERT_EQUAL(0, is_num_circle(end, &end, ",", &y));
+    ASSERT_EQUAL(0, strcmp(" 25)", end));
+
+    ASSERT_EQUAL(0, is_num_circle(end, &end, ")", &r));
+    ASSERT_EQUAL(0, strcmp("", end));
+
+    ASSERT_EQUAL(1, x);
+    ASSERT_EQUAL(5, y);
+    ASSERT_EQUAL(25, r);
+}
+
+CTEST(TEST_parse_circle, simple_parse_circle)
+{
+    /*                               ._.            */
+    char cool_circle[] = "cIrClE(1        5,     25)";
+    char bad_circle[] = "cirlce(1 5, 25)";
+    char very_bad_circle[] = "circle(1 5, 25)f";
+    Circle values;
+    ASSERT_EQUAL(0, parse_circle(cool_circle, &values));
+    ASSERT_EQUAL(ERROR_PARSER_NAME, parse_circle(bad_circle, &values));
+    ASSERT_EQUAL(
+            ERROR_PARSER_UNEXPECTED_TOKEN,
+            parse_circle(very_bad_circle, &values));
+    ASSERT_EQUAL(1, values.x);
+    ASSERT_EQUAL(5, values.y);
+    ASSERT_EQUAL(25, values.r);
+}
+
+CTEST(TEST_calculate_circle, simple_calculate_circle)
+{
+    Circle circle = {5, 5, 5, 0, 0, NULL};
+    calculate_circle(&circle);
+    double area = M_PI * circle.r * circle.r;
+    double perimetr = 2 * M_PI * circle.r;
+    ASSERT_EQUAL(area, circle.area);
+    ASSERT_EQUAL(perimetr, circle.perimetr);
+}
+
+CTEST(TEST_get_intersects_circles, simple_get_intersects_circles)
+{
+    Circle* circles = (Circle*)malloc(sizeof(Circle) * 3);
+    circles[0].x = 0;
+    circles[0].y = 0;
+    circles[0].r = 5;
+
+    circles[1].x = 2;
+    circles[1].y = 2;
+    circles[1].r = 1;
+
+    circles[2].x = 0;
+    circles[2].y = 0;
+    circles[2].r = 1;
+
+    get_intersects_circles(circles, 3);
+    /*/             1 : 2, 3               /*/
+    ASSERT_EQUAL(0, circles[0].intersects[0]);
+    ASSERT_EQUAL(1, circles[0].intersects[1]);
+    ASSERT_EQUAL(1, circles[0].intersects[2]);
+    /*/             2 : 1                  /*/
+    ASSERT_EQUAL(1, circles[1].intersects[0]);
+    ASSERT_EQUAL(0, circles[1].intersects[1]);
+    ASSERT_EQUAL(0, circles[1].intersects[2]);
+    /*/             3 : 1                  /*/
+    ASSERT_EQUAL(1, circles[2].intersects[0]);
+    ASSERT_EQUAL(0, circles[2].intersects[1]);
+    ASSERT_EQUAL(0, circles[2].intersects[2]);
+
+    free_circles(circles, 3);
 }
